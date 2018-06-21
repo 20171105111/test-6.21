615b2f45164d69887db086f871b2e36a5a05b300
20171105111 <2604406667@qq.com>
2018-06-21 09:56:23 +0800

    6.21

--- test 6.21/main.cpp
+++ test 6.21/main.cpp
@@ -8,8 +8,9 @@
 
 #include <iostream>
 using namespace std;
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int i;
+    for(i=0;i<100;i++)
+       cout<<i<<endl;
     return 0;
 }
