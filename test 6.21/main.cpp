5af7f9d182d2f4d1b4a7496388439555c57d62fa
徐涛 <s20171105111@mac26.local>
2018-06-21 09:29:50 +0800

    Initial Commit

---
+++ test 6.21/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test 6.21
+//
+//  Created by 徐涛 on 18/6/21.
+//  Copyright © 2018年 徐涛. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
