daaa46c4d72db90abed2e0cc747b9839ec2fe899
20171105111 <2604406667@qq.com>
2018-06-26 14:48:14 +0800

    6.25

---
+++ test 6.25/test 6.25/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test 6.25
+//
+//  Created by 徐涛 on 18/6/26.
+//  Copyright © 2018年 徐涛. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
